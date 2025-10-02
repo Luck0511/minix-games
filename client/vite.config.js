e8ff0efd16f4f9d7041647ebf92885aa23171224
Luc <lucaterra05@gmail.com>
2025-10-02 14:13:29 +0200

    CLIENT: eliminated last typescript residue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@shared': fileURLToPath(new URL('../shared', import.meta.url))
-    }
-  }
+  plugins: [react()]
 })
