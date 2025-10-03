2a25a71e879ecd9112cefca106877aa65e61fb54
Luc <lucaterra05@gmail.com>
2025-10-03 16:48:28 +0200

    SERVER: cleaning app.js file, removing unnecessary code for API

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,8 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
-import {getAllPlayers, getPlayerByName} from "./controllers/playerController.js";
-import {db} from "./models/index.js";
 
-const app = express();
+export const app = express();
 export const server = createServer(app);
 
 const io = new Server(server, {
@@ -24,55 +22,6 @@ app.get('/', (req, res) => {
         message : "Multiplayer game server is running and responsive!",
     });
 });
-
-app.get('/allplayers', async (req, res) => {
-    const allPlayers = await getAllPlayers();
-    res.json({allPlayers});
-})
-
-app.get('/sampleseeding', (req, res) => {
-    const {Player} = db;
-    Player.create({playerName: 'sampleUser1', password: 'hashedpassword123'}).then(()=>{
-        console.log('Created sample Player')
-    })
-    res.json("executed sample seeding");
-})
-
-//!!! Testing get player by name
-app.get('/getPlayer', async (req, res) => {
-    const playerName = req.query.playerName;
-    if(!playerName){
-        return res.status(400).json({message: 'Player name is required'});
-    }
-    const player = await getPlayerByName(playerName);
-    res.json({message:'Player found', playerInfo: player});
-})
-
-//!!! Testing player registration
-app.post('/registerPlayer', async (req, res) => {
-    const {Player} = db;
-    console.log(req.body);
-    const {playerName, password} = req.body;
-    if(!playerName || !password){
-        return res.status(400).json({message: 'Player name and password are required'});
-    }
-    try{
-        const existingPlayer = await Player.findOne({
-            where: {
-                playerName
-            }
-        });
-        if(existingPlayer){
-            return res.status(409).json({message: 'A Player with this name already exists'});
-        }else {
-            const newPlayer = await Player.create({playerName, password});
-            return res.status(200).json({message: 'Player profile created successfully', playerInfo: newPlayer});
-        }
-    }catch (err){
-        console.error('Error in player registration:', err)
-        return res.status(500).json({message: 'Internal server error'});
-    }
-})
 
 // Socket.io connection handling --> SOCKET MANAGEMENT
 io.on('connection', (clientSocket) => {
