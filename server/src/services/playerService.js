8b79a1dcc2210e84ab427a7b00dd85a43e306749
Luc <lucaterra05@gmail.com>
2025-10-03 16:44:34 +0200

    SERVER: moving file to service folder, adding registration service

---
+++ server/src/services/playerService.js
@@ -0,0 +1,60 @@
+// controller for player related operation trough API
+import {db} from '../models/index.js';
+import {Player} from "../models/Player.js";
+import {passwordHash} from "./authService.js";
+
+export const getAllPlayers = async () => {
+    try{
+        const {Player} = db;
+        return await Player.findAll();
+    }catch(err){
+        console.error('Error in DB query:', err)
+        return null;
+    }
+}
+
+export const getPlayerByName = async (username) => {
+    if(!username){
+        console.error('No username provided');
+        return null;
+    }
+    try{
+        const {Player} = db;
+        return await Player.findOne({
+            where: {
+            playerName: username
+        }});
+    }catch (err){
+        console.error('Error in player login:', err)
+        return null;
+    }
+}
+
+export const registerNewPlayer = async (playerName, password) => {
+    if(!playerName || !password){
+        return 400; //{message: 'Player name and password are required'}
+    }
+    try{
+        //check if playerName is already taken
+        const existingPlayer = await Player.findOne({
+            where: {
+                playerName
+            }
+        });
+        if(existingPlayer){
+            return 409 //{message: 'A Player with this name already exists'};
+        }else {
+            const hashedPassword = await passwordHash(password)
+            const newPlayer = await Player.create({playerName, hashedPassword});
+            return {opStatus: 200, newPlayer: newPlayer}
+            //{message: 'Player profile created successfully', playerInfo: newPlayer});
+        }
+    }catch (err){
+        console.error('Error in player registration:', err)
+        return 500 //{message: 'Internal server error'};
+    }
+}
+
+export const loginPlayer = async (playerName, password) => {
+
+}
