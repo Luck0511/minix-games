a88ad2278139c9379a209ce33601c464118da0a9
Luc <lucaterra05@gmail.com>
2025-10-04 22:40:40 +0200

    SERVER: added authorization cookies setter, allows to add a cookie containing the JWT token to the response to a request like registration and/or login

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -4,14 +4,32 @@ import bcrypt from 'bcrypt';
 //other imports
 import {appConfig} from "../config/config.js";
 
-export const passwordHash = async (password) => {
-    const saltRounds = appConfig.auth.bcryptRounds
-    return await bcrypt.hash(password, saltRounds);
-}
+/*==== COOKIES ====*/
 
-export const verifyPassword = async (password, hashedPassword) => {
-    return await bcrypt.compare(password, hashedPassword);
-}
+    //sets cookie to include with response, secure and dynamic based on .env file and production env
+    export const setAuthCookie = (res, token) => {
+        const isProduction = appConfig.app.env === 'production';
+
+         return res.cookie('token', token, {
+            httpOnly: true,
+            secure: isProduction,
+            sameSite: isProduction ? 'strict' : 'lax',
+            maxAge: appConfig.auth.cookieMaxAge
+        })
+    }
+
+
+/*==== PASSWORDS ====*/
+
+    //hashes password
+    export const passwordHash = async (password) => {
+        const saltRounds = appConfig.auth.bcryptRounds
+        return await bcrypt.hash(password, saltRounds);
+    }
+    //verifies password hashed and input password
+    export const verifyPassword = async (password, hashedPassword) => {
+        return await bcrypt.compare(password, hashedPassword);
+    }
 
 //generate JWT token
 export const generateJWT = (payload) => {
