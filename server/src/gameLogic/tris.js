bb51a1df022b373b04257611277b02ade93478ce
Luc <lucaterra05@gmail.com>
2025-12-07 17:49:08 +0100

    FIX in SERVER: modified the tris game class to follow plain javascript syntax

--- server/src/gameLogic/tris.js
+++ server/src/gameLogic/tris.js
@@ -1,22 +1,44 @@
 import {Player} from "../models/Player.js";
 
-class trisGame {
-    player1: Player;
-    player2: Player;
-    boardSize: number;
-    board:(null | string)[][];
-    winner: Player | null;
-
-    constructor(player1, player2, boardSize=3){
+export class TrisGame {
+    constructor(player1, player2, boardSize = 3) {
+        this.lobbyId = null;
         this.player1 = player1;
         this.player2 = player2;
         this.boardSize = boardSize;
-        this.board = Array(boardSize).fill(null).map(()=>Array(boardSize).fill(null));
+        this.board = Array(boardSize).fill().map(() => Array(boardSize).fill(null));
+        this.winningCondition = false;
         this.winner = null;
+    }
+
+    setWinner(winner) {
+        this.winningCondition = true;
+        this.winner = winner;
     }
 }
 
-export const gameInit = ()=>{
-
+export const trisInit = (player1, player2, boardSize) => {
+    const game = new TrisGame(player1, player2, boardSize);
+    return game;
 }
 
+export const move = (x, y, player, trisGame) => {
+    switch (player) {
+        // player1 assigned to 'O'
+        case trisGame.player1: {
+            trisGame.board[y][x] = 'O';
+            console.log(`${trisGame.lobbyId}: placed 'O' in ${x}, ${y}`);
+            break;
+        }
+        // player2 assigned to 'X'
+        case trisGame.player2: {
+            trisGame.board[y][x] = 'X';
+            console.log(`${trisGame.lobbyId}: placed 'X' in ${x}, ${y}`);
+            break;
+        }
+        default: {
+            console.error(`${trisGame.lobbyId}: couldn't find the player who made the move!`)
+            return;
+        }
+    }
+}
